c7b624498a2147cdb233d4f83e5b89b3961654cc
HollySte <h.stemp@unsw.edu.au>
2022-02-04 12:20:54 +0100

    Added RSS feeds

--- send_email/jounal_feed_parsers/__init__.py
+++ send_email/jounal_feed_parsers/__init__.py
@@ -4,6 +4,7 @@ from .science_parser import parse_science_feed
 
 RSS_feed_parsers = {
     'arXiv quant-ph': (parse_arxiv_feed, 'http://export.arxiv.org/rss/quant-ph'),
+    'arXiv cond-mat': (parse_arxiv_feed, 'http://export.arxiv.org/rss/cond-mat'),
 
     'Nature': (parse_nature_feed, 'http://feeds.nature.com/nature/rss/current'),
     'Nature Chemistry': (parse_nature_feed, 'http://feeds.nature.com/nchem/rss/current'),
@@ -11,6 +12,9 @@ RSS_feed_parsers = {
     'Nature Materials': (parse_nature_feed, 'http://feeds.nature.com/nmat/rss/current'),
     'Nature Nanotechnology': (parse_nature_feed, 'http://feeds.nature.com/nnano/rss/current'),
     'Nature Physics': (parse_nature_feed, 'https://www.nature.com/nphys.rss'),
+    'Nature Reviews Physics': (parse_nature_feed, 'http://feeds.nature.com/natrevphys/rss/current'),
+    'Nature Communications': (parse_nature_feed, 'http://feeds.nature.com/ncomms/rss/current'),
+    'npj Quantum Information': (parse_nature_feed, 'http://feeds.nature.com/npjqi/rss/current'),
 
     'Science': (parse_science_feed, 'http://science.sciencemag.org/rss/express.xml'),
     'Science Advances': (parse_science_feed, 'http://advances.sciencemag.org/rss/current.xml'),
