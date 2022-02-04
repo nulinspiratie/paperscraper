bbfe68207161209e53e063be26d2d3c4bf27721b
HollySte <h.stemp@unsw.edu.au>
2022-02-04 13:02:22 +0100

    Added Physical review feed

--- send_email/jounal_feed_parsers/__init__.py
+++ send_email/jounal_feed_parsers/__init__.py
@@ -1,6 +1,7 @@
 from .arxiv_parser import parse_arxiv_feed
 from .nature_parser import parse_nature_feed
 from .science_parser import parse_science_feed
+from .physical_review_parser import parse_physical_review_feed
 
 RSS_feed_parsers = {
     'arXiv quant-ph': (parse_arxiv_feed, 'http://export.arxiv.org/rss/quant-ph'),
@@ -18,4 +19,18 @@ RSS_feed_parsers = {
 
     'Science': (parse_science_feed, 'http://science.sciencemag.org/rss/express.xml'),
     'Science Advances': (parse_science_feed, 'http://advances.sciencemag.org/rss/current.xml'),
+
+    'Physical Review Letters': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prl.xml'),
+    'Physical Review X': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prx.xml'),
+    'Physical Review Quantum': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prxquantum.xml'),
+    'Reviews of Modern Physics': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/rmp.xml'),
+    'Physical Review Applied': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prapplied.xml'),
+    'Physical Review Materials': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prmaterials.xml'),
+    'Physical Review Research': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prresearch.xml'),
+    'Physical Review A': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/pra.xml'),
+    'Physical Review B': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prb.xml'),
+    'Physical Review C': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prc.xml'),
+    'Physical Review D': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/prd.xml'),
+    'Physical Review E': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/pre.xml'),
+    'Physical Review Physics': (parse_physical_review_feed, 'http://feeds.aps.org/rss/recent/physics.xml'),
 }
--- send_email/jounal_feed_parsers/physical_review_parser.py
+++ send_email/jounal_feed_parsers/physical_review_parser.py
@@ -7,24 +7,36 @@ from pprint import pformat
 logger = logging.getLogger(__name__)
 
 
-def parse_arxiv_feed(url, journal):
+def parse_physical_review_feed(url, journal):
     from ..paper_tools import Paper, Author
 
     rss_dict = feedparser.parse(url)
     papers = []
     for entry in rss_dict['entries']:
         try:
-            author_strings = re.findall('>([A-Z][^<]+)<\/a>', entry['author'])
+            # Extract authors
+            author_entry = entry['author'].replace('\u2009', ' ')
+            if '<em>et al.' in author_entry:
+                author_entry = author_entry.split('</em>')[0]
+                author_entry = author_entry.replace('<em>', ', ')
+            author_strings = author_entry.split(', ')
             authors = [Author(author_string) for author_string in author_strings]
+
             title = entry['title'].split(' (arXiv')[0]
+            if '<p>' in entry['summary']:
+                abstract = entry['summary'].split('<p>')[1].split('</p>')[0]
+            else:
+                abstract = ''
+
             paper = Paper(authors=authors,
                           title=title,
-                          abstract=entry['summary'].strip('<p>').strip('</p'),
+                          abstract=abstract,
                           link=entry['link'],
                           journal=journal,
                           pdf_link=entry['link'].replace('/abs/', '/pdf/'),
                           date=rss_dict['updated_parsed'])
             papers.append(paper)
-        except Exception:
+        except Exception as e:
             logger.error(f'Could not parse {journal} entry {pformat(entry)}')
+            print(e)
     return papers
